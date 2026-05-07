8b3a95d2b867416fc9b3d9472167bcf176ab6933
Benito <benissj4@gmail.com>
2026-05-07 23:50:53 +0200

    cc

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Práctica Git</title>
-</head>
-<body>
-    <h1>Hola mundo, soy Antonio, que tal</h1>
-</body>
-</html>
+<?php
+echo "Hola Mundo Antonio"
+?>
